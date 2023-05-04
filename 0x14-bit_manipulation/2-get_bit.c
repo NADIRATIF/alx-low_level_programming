6d0bc698e7e22b734b0df8f6a36533c223566d54
nadir <nadir.atif.94@gmail.com>
2023-05-04 09:41:34 -0700

    prototype tasks 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,13 +7,13 @@
  *
  * Return: the value of the bit at index
  */
-int get_bit(unsigned long int n, unsigned int i)
+int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int d, r;
 
-	if (i > (sizeof(unsigned long int) * 8 - 1))
+	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
-	d = 1 << i;
+	d = 1 << index;
 	r = n & d;
 	if (r == d)
 		return (1);
