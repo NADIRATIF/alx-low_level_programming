22eb3af7b050add952c7cd31914365ffab883076
nadir <nadir.atif.94@gmail.com>
2023-05-04 09:45:19 -0700

    prototype tasks 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,13 +7,13 @@
  * Return: 1 if it worked, or -1 if an error occurred
  */
 
-int clear_bit(unsigned long int *n, unsigned int i)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int x;
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
-	x = ~(1 << i);
+	x = ~(1 << index);
 	*n = *n & x;
 
 	return (1);
