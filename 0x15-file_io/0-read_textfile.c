254a10f9e9ae29dc5c0b9af16f1d34000d961523
nadir <nadir.atif.94@gmail.com>
2023-05-08 06:30:11 -0700

    tasks 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - function that reads a text file 
+ * and prints it to the POSIX standard output
+ * @filename: file name pointer.
+ * @letters: number of letters.
+ *
+ * Return: the function fails or filename is NULL - 0.
+ *         O/w - actual number of bytes can read and print.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t op, rd, wr;
+	char *b;
+
+	if (filename == NULL)
+		return (0);
+
+	b = malloc(sizeof(char) * letters);
+	if (b == NULL)
+		return (0);
+
+	op = open(filename, O_RDONLY);
+	rd = read(o, buffer, letters);
+	wr = write(STDOUT_FILENO, buffer, r);
+
+	if (op == -1 || rd == -1 || wr == -1 || wr != rd)
+	{
+		free(b);
+		return (0);
+	}
+
+	free(b);
+	close(op);
+
+	return (wr);
+}
